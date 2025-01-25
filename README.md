238f0b27ac59490277affaf7c99776c012a451fc
d-saikrishna <saikrishnadammalapati@gmail.com>
2025-01-25 20:47:00 +0530

    UPDATE Ragchatbot - feedback

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # SamvidhanBot
-Tweeting the wisdom of the Constitution of India and its makers.
+Wisdom of the Constitution of India and its makers.
 
-[Link](https://twitter.com/SamvidhanBot)
+# RAG ChatBot
+
+The RAG Chatbot helps users talk to the makers of the Indian Constitution. This is deployed using Streamlit here: [https://constitutionbot.streamlit.app/](https://constitutionbot.streamlit.app/)
+
+The code for this is app is here: `ragchatbot.py`
+
+I detailed the process I followed in this blog: [Medium](https://medium.com/@saikrishna_17904/e9b75282c54f)
+
+Please feel free to give feedback on improving this. Very happy to work with others as well. Make a PR.
+
+Few more things that have to be done:
+- [ ] Conversation instead of a single query.
+- [ ] Add more LLM Models.
+- [ ] Train Committee reports datasets as well.
+
+
+# Twitter bot
+
+[SamvidhanBot](https://twitter.com/SamvidhanBot)
 
 Curated by [Maximusden](https://twitter.com/Maximusden) and [Sai Krishna](https://github.com/d-saikrishna)
 
@@ -9,12 +27,7 @@ The debates of the Constituent Assembly, the articles in the constitution & the 
 
 Requesting citizens to add context wherever necessary and be part of this discussion.
 
-# Blogs
-1. [Scientific Temper in India](https://samvidhanbot.substack.com/p/scientific-temper-in-india): Article on Scientific temper in India, mourning the loss of Science martyrs.  
-2. [Appointing the umpire](https://samvidhanbot.substack.com/p/appointing-the-umpire): Article on the Supreme Court's judgement on the appointment of the Cheif Election Commissioner. Data and Visualisations can be found at: [Link](https://github.com/d-saikrishna/SamvidhanBot/tree/main/Blogs/ElectionCommission)
-
-
-# How to build the bot?
+## How to build this twitter bot?
 
 1. Register a Project app on the [developers portal](https://developer.twitter.com/en/portal/dashboard). Free version is enough for my app.
 2. Get API_KEY, API_SECRET, ACCESS_TOKEN ACCESS_TOKEN_SECRET from the developers portal for the app. You'd use them for authentication. Save them in the environment variables. 
@@ -38,3 +51,7 @@ media_upload_url = 'https://upload.twitter.com/1.1/media/upload.json'
         /bin/bash /path/to/your/script.sh
 
     I had created a run_bot.sh shell script which hsa the workflow coded.
+
+# Blogs
+1. [Scientific Temper in India](https://samvidhanbot.substack.com/p/scientific-temper-in-india): Article on Scientific temper in India, mourning the loss of Science martyrs.  
+2. [Appointing the umpire](https://samvidhanbot.substack.com/p/appointing-the-umpire): Article on the Supreme Court's judgement on the appointment of the Cheif Election Commissioner. Data and Visualisations can be found at: [Link](https://github.com/d-saikrishna/SamvidhanBot/tree/main/Blogs/ElectionCommission)
--- ragchatbot.py
+++ ragchatbot.py
@@ -7,8 +7,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_huggingface import HuggingFaceEndpoint
-from langchain.callbacks.tracers.langchain import wait_for_all_tracers
-
 
 import streamlit as st
 import os
@@ -18,6 +16,21 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+from langchain.callbacks import LangChainTracer
+from langsmith import Client
+langsmith_key = os.getenv("LANGSMITH_API_KEY")
+
+callbacks = [
+LangChainTracer(
+  project_name="cad",
+  client=Client(
+    api_url="https://api.smith.langchain.com",
+    api_key=langsmith_key
+  )
+)
+]
+
+
 
 from dotenv import load_dotenv
 # Load environment variables from the .env file
@@ -25,8 +38,12 @@ load_dotenv()
 
 # Access the API key
 hf_key = os.getenv("HF_TOKEN")
+from PIL import Image
+im = Image.open("assets/favicon-32x32.png")
 
-st.set_page_config(layout="wide", page_title="Samvidhan")
+st.set_page_config(
+    page_title="CAD",
+    page_icon=im)
 # Following line will help in seeing dynamic session states
 # "st.session_state object:" , st.session_state
 
@@ -43,7 +60,8 @@ db_disk = Chroma(
 
 #Design Prompt Template
 prompt = ChatPromptTemplate.from_template("""Answer the following question strictly based on provided context.
-                                          The context is the debates from the Constitutional Assembly of India.
+                                          The context provided is the debates from the Constitutional Assembly of India.
+                                          Give the answer in bullet points. Include quotes if necessary.
                                           Execute Python codes if needed to answer for any calculations, don't print code in the answer.
                                           <context>
                                           {context}
@@ -51,8 +69,23 @@ prompt = ChatPromptTemplate.from_template("""Answer the following question stric
                                           Question: {input}""")
 
 
+st.title("Talk to the people who made Indian Constitution!")
 #model = OllamaLLM(model="llama3.2")
-repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+models = ['MistralAI', 'Google Gemma']
+llm_model = st.selectbox("Select a LLM Model: ", models, placeholder ="Choose LLM Model")
+
+questions = ["Custom query"]
+with open(join("questions.txt")) as f:
+    questions += f.read().split("\n")
+
+query = st.selectbox("Select a query: ", questions, index=None, placeholder ="Choose Custom query to ask your own question")
+
+if llm_model=="MistralAI":
+    repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+elif llm_model=="Google Gemma":
+    repo_id = "google/gemma-2-2b-it"
+else:
+    repo_id = "mistralai/Mistral-7B-Instruct-v0.3"#"deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 
 model = HuggingFaceEndpoint(repo_id = repo_id, max_length=1000, temperature=0.5,token=hf_key)
 
@@ -62,15 +95,7 @@ retriever = db_disk.as_retriever(search_type="mmr",
                                                 })
 
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
-
-questions = ["Custom query"]
-with open(join("questions.txt")) as f:
-    questions += f.read().split("\n")
-
-st.title("Talk to the people who made Constitution!")
 answer=''
-
-query = st.selectbox("Select a query: ", questions, index=None, placeholder ="Choose Custom query to ask your own question")
 
 if query == "Custom query":
     query = st.text_input("Ask your question",)
@@ -82,9 +107,23 @@ with st.form(key='my_form_to_submit'):
 if submit_button:
     with st.spinner('Wait for it...'):
         time.sleep(5)
-        try: 
-            response = retrieval_chain.invoke({"input":query})
-        finally:
-            wait_for_all_tracers()
+        config={"callbacks": callbacks}
+        response = retrieval_chain.invoke({"input":query}, config=config)
         answer = response['answer']
     st.write(answer)
+
+# Footer
+st.markdown("""
+        <h10 style="text-align: center;
+                    float: left;
+                    bottom: 3rem;">
+            Follow <a href="https://x.com/SamvidhanBot"> SamvidhanBot</a> on X <br>
+            </h10>""",
+            unsafe_allow_html=True)
+st.markdown("""
+        <h10 style="text-align: center;
+                    float: right;
+                    bottom: 3rem;">
+            Give a ⭐ on <a href="https://github.com/d-saikrishna/SamvidhanBot"> GitHub</a>
+            </h10>""",
+            unsafe_allow_html=True)
